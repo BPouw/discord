b03030d57cbe021b826cc67eee708fde17555629
Boris Pouw <boris.pouw@student.avans.nl>
2023-01-02 20:11:40 +0100

    bug fix

--- commands/dm_me.js
+++ commands/dm_me.js
@@ -5,6 +5,7 @@ module.exports = {
 		.setName('dm_me')
 		.setDescription('The dragon will send you a personal message'),
 	async execute(interaction) {
+		await interaction.reply('The dragon will DM you now 😏')
 		await interaction.user.send('You requested personal time with the dragon? 🐲 😏 🔥')
 	},
 };
