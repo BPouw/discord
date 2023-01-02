de09429acc18bedda34fe9b08ef3d35a6579afa1
Boris Pouw <boris.pouw@student.avans.nl>
2023-01-02 13:36:45 +0100

    the dragon can now DM

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Discord
+
+This is a small hobby project created using node and discord.js
+
+Created by Boris Pouw for the dragon energy discord server
---
+++ commands/dm_me.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('dm_me')
+		.setDescription('The dragon will send you a personal message'),
+	async execute(interaction) {
+		await interaction.user.send('You requested personal time with the dragon? 🐲 😏 🔥')
+	},
+};
