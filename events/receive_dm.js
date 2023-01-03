ac0979af770c0426e0fe497123a81d66eacffdc0
boris.pouw <bj.pouw@student.avans.nl>
2023-01-03 16:01:14 +0100

    openAI integration

--- events/receive_dm.js
+++ events/receive_dm.js
@@ -1,10 +1,26 @@
 const { Events } = require('discord.js');
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
 
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(interaction) {
 		if (interaction.guild) return;
-        await interaction.author.send("The dragon has nothing to discuss with you, for all my commands I refer you back to dragon energy")
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "The following is a conversation with a wise dragon. The dragon is smart, flirty, and friendly and answers questions in a single response \nHuman: " + interaction.content + "\n",
+            temperature: 0.9,
+            max_tokens: 150,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0.6,
+            stop: [" Human:", " Dragon:"],
+          });
+        await interaction.author.send(response.data.choices[0].text)
         .catch(() => {
             console.log('Not authorized to DM this user')
         }) 
