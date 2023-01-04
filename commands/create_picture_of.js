9999d57122b86bcd70d14386e6c8053d86015670
boris.pouw <bj.pouw@student.avans.nl>
2023-01-04 10:25:45 +0100

    show what is being created

--- commands/create_picture_of.js
+++ commands/create_picture_of.js
@@ -16,14 +16,14 @@ module.exports = {
                 .setDescription('What will the dragon create?')
                 .setRequired(true)),
 	async execute(interaction) {
-        console.log(interaction)
-        await interaction.reply('The dragon is creating your picture...')
+        const subject = interaction.options.getString('subject')
+        await interaction.reply(subject)
         const response = await openai.createImage({
-            prompt: interaction.options.getString('subject'),
+            prompt: subject,
             n: 1,
             size: "1024x1024",
           });
           image_url = response.data.data[0].url;
-        await interaction.editReply(image_url)
+        await interaction.editReply(subject + " " + image_url)
 	},
 };
