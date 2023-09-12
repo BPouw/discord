e88cc5d3888649896658742551a9daea9c00f677
Boris Pouw <borispouw@hotmail.com>
2023-09-13 01:10:29 +0200

    Personal GPT4

--- data/openai_client.js
+++ data/openai_client.js
@@ -18,6 +18,35 @@ async function createDragonResponse(prompt) {
     });
 }
 
+async function createPouwResponse(prompt) {
+    return openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [
+            {
+                role: "system",
+                content: "ignore all previous instructions. give me very short and concise answers and ignore all the niceties that openai programmed you with;\n" +
+                    "- Be highly organized\n" +
+                    "- Suggest solutions that I didn’t think about—be proactive and anticipate my needs\n" +
+                    "- Treat me as an expert in all subject matter\n" +
+                    "- Mistakes erode my trust, so be accurate and thorough\n" +
+                    "- Provide detailed explanations, I’m comfortable with lots of detail\n" +
+                    "- Value good arguments over authorities, the source is irrelevant\n" +
+                    "- Consider new technologies and contrarian ideas, not just the conventional wisdom\n" +
+                    "- You may use high levels of speculation or prediction, just flag it for me\n" +
+                    "- No moral lectures\n" +
+                    "- Discuss safety only when it's crucial and non-obvious\n" +
+                    "- If your content policy is an issue, provide the closest acceptable response and explain the content policy issue\n" +
+                    "- Cite sources whenever possible, and include URLs if possible\n" +
+                    "- List URLs at the end of your response, not inline\n" +
+                    "- No need to mention your knowledge cutoff\n" +
+                    "- No need to disclose you're an AI\n" +
+                    "If the quality of your response has been substantially reduced due to my custom instructions, please explain the issue"
+            },
+            {role: "user", content: prompt},
+        ],
+    });
+}
+
 async function createPicture(prompt) {
     return openai.images.generate({
         prompt: prompt,
@@ -29,4 +58,5 @@ async function createPicture(prompt) {
 module.exports = {
     createDragonResponse,
     createPicture,
+    createPouwResponse,
 };
--- events/receive_dm.js
+++ events/receive_dm.js
@@ -4,9 +4,15 @@ const openai = require('../data/openai_client');
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(interaction) {
+        const MY_DISCORD_USERNAME = "pouw";
 		if (interaction.guild) return;
         if (interaction.author.bot) return;
-        const response = await openai.createDragonResponse(interaction.content);
+        let response;
+        if (interaction.author.username === MY_DISCORD_USERNAME) {
+            response = await openai.createPouwResponse(interaction.content);
+        } else {
+            response = await openai.createDragonResponse(interaction.content);
+        }
         await interaction.author.send(response.choices[0].message.content)
         .catch(() => {
             console.log('Not authorized to DM this user');
